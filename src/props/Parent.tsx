b3e0851ecd24e38518534a0ea7c1aba5b76a6f9c
Rohan Raj Gautam <rohangautamdev@gmail.com>
2021-05-16 08:09:29 +0545

    working with state

--- src/props/Parent.tsx
+++ src/props/Parent.tsx
@@ -1,11 +1,7 @@
 import { ChildAsFC } from "./Child";
 
 const Parent = () => {
-  return (
-    <ChildAsFC color="yellow" onClick={() => console.log("Clicked")}>
-      children value
-    </ChildAsFC>
-  );
+  return <ChildAsFC color="yellow" onClick={() => console.log("Clicked")} />;
 };
 
 export default Parent;
