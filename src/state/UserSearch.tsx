ad05b338f33fc969be45e0ff60f16f81033eb697
Rohan Raj Gautam <rohangautamdev@gmail.com>
2021-05-16 10:01:30 +0545

    focus on input text at start

--- src/state/UserSearch.tsx
+++ src/state/UserSearch.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useRef, useEffect } from "react";
 
 const users = [
   { name: "Sarah", age: 20 },
@@ -7,8 +7,13 @@ const users = [
 ];
 
 const UserSearch: React.FC = () => {
+  const inputRef = useRef<HTMLInputElement | null>(null);
   const [name, setName] = useState("");
   const [user, setUser] = useState<{ name: string; age: number } | undefined>();
+
+  useEffect(() => {
+    inputRef.current?.focus();
+  }, []);
 
   const onClick = () => {
     const foundUser = users.find((user) => {
@@ -18,10 +23,14 @@ const UserSearch: React.FC = () => {
     setUser(foundUser);
   };
 
+  const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setName(event.target.value);
+  };
+
   return (
     <div>
       User Search
-      <input value={name} onChange={(e) => setName(e.target.value)} />
+      <input ref={inputRef} value={name} onChange={onChange} />
       <button onClick={onClick}>Find User</button>
       <div>
         Name: {user?.name}
